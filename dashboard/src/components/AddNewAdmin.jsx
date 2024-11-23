49aacad61dab5491397c9d6d2c8be6c5da815b30
Dharm-Singh-45 <dharmjat45@gmail.com>
2024-11-23 20:29:29 +0530

    backend url add into dashboard

--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/admin/addnew",
+          "https://hospital-management-qj6g.onrender.comapi/v1/user/admin/addnew",
           { firstName, lastName, email, phone, aadharNumber, dob, gender, password },
           {
             withCredentials: true,
--- dashboard/src/components/Messages.jsx
+++ dashboard/src/components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/message/getall",
+          "https://hospital-management-qj6g.onrender.comapi/v1/message/getall",
           { withCredentials: true }
         );
         setMessages(data.messages);
